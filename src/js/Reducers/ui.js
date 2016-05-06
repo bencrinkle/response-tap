0716ffa66f471cd59f7b217805218ec7ef18c492
Ben Jones <benjones1985@gmail.com>
2016-05-06 11:42:59 +0100

    changed create button colour and fixed mutated tab index

--- src/js/Reducers/ui.js
+++ src/js/Reducers/ui.js
@@ -5,7 +5,7 @@ const initialState = {
 const ui = (state = initialState, action) => {
 	switch(action.type){
 		case 'UPDATE_TAB':
-			var newTab = ++state.tab;
+			var newTab = state.tab + 1;
 			return Object.assign({}, state, {'tab': newTab});
 		default:
 			return state;
